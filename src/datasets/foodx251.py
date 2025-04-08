b2e90a058e5f7ff96b984737464d8e7bfe72f3cb
Aidan-B1409 <aidanb1049@gmail.com>
2025-04-07 19:09:30 -0700

    Add naive pattern matching objective for classification accuracy

---
+++ src/datasets/foodx251.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+
+# root
+# --- annot
+# --- --- class_list.txt
+# --- --- test_info.csv
+class FoodX251():
+    def __init__(self, root: str) -> None:
+        self.root = root
+        self.cmap = self.get_classmap()
+        self.train_path = "annot/train_info.csv"
+        self.test_path = "annot/test_info.csv"
+        self.val_path = "annot/val_info.csv"
+
+    def _naturalize_class_labels(self, row: str) -> str:
+        return row.capitalize().replace('_', ' ')
+
+    def get_classmap(self) -> pd.DataFrame:
+        cmap = pd.read_csv("annot/class_list.txt", delim_whitespace=True, index_col=0, names=['id', 'label'])
+        cmap['label'] = cmap['label'].apply(self._naturalize_class_labels)
+        return cmap
+
+    def _map_class(self, row: str):
+        return self.cmap.loc[int(row)]['label']
+
+    def _get_dataset(self, pth: str):
+        data = pd.read_csv(pth, names=['fname', 'class'])
+        # substitute id numbers with class names
+        data['class'] = data['class'].apply(self._map_class)
+        return data
+
+    def test_set(self) -> pd.DataFrame:
+        return self._get_dataset(self.test_path)
---
+++ src/evaluate.py
@@ -0,0 +1,28 @@
+from .datasets.foodx251 import FoodX251
+from .intern import InternVLM
+
+ds_path = 'compost3.jpg'
+
+# goal: 
+# for every image in foodx-251
+# read class label, image 
+
+model = InternVLM("InternVL2_5-8B-MPO")
+ds_path = "/nfs/guille/eecs_research/soundbendor/beerya/food_cap_datasets/FoodX-251"
+dataset = FoodX251(ds_path)
+prompt = '<image>\nPlease classify the food item in this image. Answer with a single item if possible.'
+
+test_set = dataset.test_set()
+# only has class names
+
+accuracy = 0
+for i, row in test_set.iterrows():
+    response = model.infer(f"{ds_path}/test/test_set/{row['fname']}", prompt)
+    # TODO: pattern match on label from row['class'] to compute accuracy
+    print(f"Item: {row['class']}")
+    print(response)
+    if row['fname'] in response:
+        accuracy += 1
+
+accuracy = accuracy / len(test_set)
+
