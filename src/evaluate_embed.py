ce11d3eb95e3cbe9b91b1b8e6f6dd09145d25fb8
Aidan-B1409 <aidanb1049@gmail.com>
2025-04-13 14:50:06 -0700

    Add top1, top5 scoring metrics to evaluation loop

--- src/evaluate_embed.py
+++ src/evaluate_embed.py
@@ -15,15 +15,25 @@ db = VectorDB("db/qdrant", "assets/embed_models/mpnet-base-food/final")
 test_set = dataset.val_set()
 # only has class names
 
-acc = 0
+top1_acc = 0
+top5_acc = 0
+top1_vote_acc = 0
 for i, row in test_set.iterrows():
     response = model.infer(f"{ds_path}/val/val_set/{row['fname']}", prompt)
     q_vecs = db.query(response)
-    prediction, score = db.score(q_vecs, row["class"], "voting")
+    prediction, vote_score = db.score(q_vecs, row["class"], "voting")
+    top1_prediction, top1_score = db.score(q_vecs, row["class"], "top1")
+    top5_prediction, top5_score = db.score(q_vecs, row["class"], "top5")
     print(f"Predicted Label: {prediction}")
     print(f"Description: {response}")
     print(f"True label: {row['class']}")
-    acc += score
+    top1_acc += top1_score
+    top5_acc += top5_score
+    top1_vote_acc += vote_score
 
-acc = acc / len(test_set)
-print(f"Final Score: {acc}")
+top1_acc = top1_acc / len(test_set)
+lop5_acc = top5_acc / len(test_set)
+top1_vote_acc = top1_vote_acc / len(test_set)
+print(f"Final Score (Top 1): {top1_acc}")
+print(f"Final Score (Top 5): {top5_acc}")
+print(f"Final Score (Voting Top 1): {top1_vote_acc}")
