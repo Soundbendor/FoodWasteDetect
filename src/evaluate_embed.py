4410bfa8234cf610a7f29d0a1f4a3dfebbedf2d3
Aidan-B1409 <aidanb1049@gmail.com>
2025-04-12 19:38:19 -0700

    Fix embedding evaluation

--- src/evaluate_embed.py
+++ src/evaluate_embed.py
@@ -1,6 +1,6 @@
-from datasets.foodx251 import FoodX251
+from ds.foodx251 import FoodX251
 from intern import InternVLM
-from db.vecdb import VectorDB
+from database.vecdb import VectorDB
 
 # goal:
 # for every image in foodx-251
@@ -10,7 +10,7 @@ model = InternVLM("../intern_fw_test/InternVL2_5-8B-MPO")
 ds_path = "/nfs/guille/eecs_research/soundbendor/beerya/food_cap_datasets/FoodX-251"
 dataset = FoodX251(ds_path)
 prompt = "<image>\nPlease describe the food item in this image in a single sentence, focusing on the visual characteristics of the food."
-db = VectorDB("db/qdrant")
+db = VectorDB("db/qdrant", "assets/embed_models/mpnet-base-food/final")
 
 test_set = dataset.val_set()
 # only has class names
@@ -18,7 +18,7 @@ test_set = dataset.val_set()
 acc = 0
 for i, row in test_set.iterrows():
     response = model.infer(f"{ds_path}/val/val_set/{row['fname']}", prompt)
-    category, confidence, q_response = db.query(response, 'voting')
+    category, confidence, q_response = db.query(response, "voting")
     print(f"\nItem: {category}")
     print(f"Description: {response}")
     print(f"True label: {row['class']}")
