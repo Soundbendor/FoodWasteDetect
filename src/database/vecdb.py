f94489bac48350809eedc5e7c39d377f02b253e7
Aidan-B1409 <aidanb1049@gmail.com>
2025-04-15 14:32:30 -0700

    Get output from qdrant start

--- src/database/vecdb.py
+++ src/database/vecdb.py
@@ -30,7 +30,7 @@ class VectorDB:
             if connection_status < 0 or response.status_code != 200:
                 # call qdrant startup script on first retry
                 if i == 0:
-                    subprocess.run(["sbatch" "start_qdrant.sbatch"], shell=True)
+                    subprocess.run(["sbatch" "start_qdrant.sbatch"], shell=True, capture_output=True)
                 print("DEBUG: Waiting 60s, contacting Qdrant server...")
                 time.sleep(60)
             else:
