df94219c5bcf460c1f5f57374b04c2ff1dc4f80c
Aidan-B1409 <aidanb1049@gmail.com>
2025-04-15 14:24:01 -0700

    Remove continue

--- src/database/vecdb.py
+++ src/database/vecdb.py
@@ -27,7 +27,6 @@ class VectorDB:
                 response = requests.get(addr)
             except requests.exceptions.ConnectionError:
                connection_status = -1
-               continue
             if response.status_code != 200 or connection_status < 0:
                 # call qdrant startup script on first retry
                 if i == 0:
