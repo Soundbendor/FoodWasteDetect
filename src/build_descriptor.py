0869aa7e3514a965b53229e04b17a7af5a9dffcb
Aidan-B1409 <aidanb1049@gmail.com>
2025-04-16 16:23:15 -0700

    Update main entrypoints

--- src/build_descriptor.py
+++ src/build_descriptor.py
@@ -6,24 +6,24 @@ from llm.describer import DescriberLLM
 
 
 def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="Intern-FW: Build Descriptor Dictionary")
-    parser.add_argument('config_file', help='Path to experiment config')
+    parser = argparse.ArgumentParser(
+        description="Intern-FW: Build Descriptor Dictionary"
+    )
+    parser.add_argument("config_file", help="Path to experiment config")
     return parser.parse_args()
 
 
-def main(config_file: str):
+def main():
     args = parse_args()
     cfg = configparser.ConfigParser()
     cfg.read(args.config_file)
 
     # start LLM
-    llm = DescriberLLM(cfg.get('Models', 'llm'))
+    llm = DescriberLLM(cfg.get("Models", "llm"))
     llm.start_server()
 
     # load dataset, get class map
-    dataset = FoodX251(cfg.get('Models', 'ds_path'))
+    dataset = FoodX251(cfg.get("Models", "ds_path"))
 
-    llm.generate_descriptors(dataset, cfg.get('Models', 'dictionary'))
+    llm.generate_descriptors(dataset, cfg.get("Models", "dictionary"))
     print("Building description dictionary")
-
-
--- src/build_vdb.py
+++ src/build_vdb.py
@@ -15,19 +15,20 @@ from database.vecdb import VectorDB
 # Test database with a query
 query = "They are delicate, pastel-pink macarons featuring crisp almond-flour meringue shells sandwiched around a sweet, creamy filling."
 
+
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Intern-FW Experiment Pipeline")
-    parser.add_argument('config_file', help='Path to experiment config')
+    parser.add_argument("config_file", help="Path to experiment config")
     return parser.parse_args()
 
 
-def main(config_file: str):
+def main():
     args = parse_args()
     cfg = configparser.ConfigParser()
     cfg.read(args.config_file)
-    dict_path = cfg.get('Models', 'dictionary')
-    embedder = EmbeddingModel(dict_path, cfg.get('Models', 'embed_model'))
-    db = VectorDB(cfg.get('Models', 'db_path'), embedder)
+    dict_path = cfg.get("Models", "dictionary")
+    embedder = EmbeddingModel(dict_path, cfg.get("Models", "embed_model"))
+    db = VectorDB(cfg.get("Models", "db_path"), embedder)
     # Goal, from dictionary, build vector database
     print("INFO: Building Database...")
     db.add(dict_path)
@@ -36,4 +37,3 @@ def main(config_file: str):
     q_vecs = db.query(query)
     prediction, _ = db.score(q_vecs, "Macaron", "voting")
     print(f"Prediction: {prediction}")
-
