d967c35d7fd8e62bc70926114309f75d93561a07
Aidan-B1409 <aidanb1049@gmail.com>
2025-05-18 18:17:53 -0700

    fix index col

--- src/caption_imgs.py
+++ src/caption_imgs.py
@@ -52,7 +52,7 @@ def merge_files():
     dfs = []
     for n in range(n_splits):
         path = f"{base_path}{n+1}.txt"
-        dfs.append(pd.read_csv(path))
+        dfs.append(pd.read_csv(path, index_col = 'idx'))
     df = pd.concat(dfs)
     print(df)
 
