a8bd2c2af9736a9ca030062bba6572a7af610fb3
Aidan-B1409 <aidanb1049@gmail.com>
2025-07-01 16:04:08 -0700

    Write bounding boxes to new directory

--- src/util/seg_to_boxes.py
+++ src/util/seg_to_boxes.py
@@ -20,17 +20,23 @@ def parse_args() -> argparse.Namespace:
 
 def main():
     args = parse_args()
-    for fname in os.listdir(args.dataset_path):
-        fpath = os.path.join(args.dataset_path, fname)
+    seg_path = os.path.join(args.dataset_path, 'labels')
+    for fname in os.listdir(seg_path):
+        fpath = os.path.join(seg_path, fname)
         segments = []
         labels = []
         with open(fpath, 'r') as file:
             for segment in file.readlines():
-                seg_label = np.fromstring(segment)
+                seg_label = np.fromstring(segment, sep=' ')
                 segments.append(seg_label[1:])
                 labels.append(seg_label[0])
         bboxes = segments2boxes([np.array(s).reshape(-1, 2) for s in segments])
-        print(bboxes)
+        with open(os.path.join(args.dataset_path, 'boxes', fname), 'w') as box_file:
+            for idx, label in enumerate(labels):
+                label_string = f"{label} {str(bboxes[idx])}\n"
+                print(label_string)
+                box_file.write(label_string)
+        
 
 main()
 
