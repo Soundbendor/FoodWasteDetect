d4a4d391e33424ff0da99282b93269854daff570
Aidan-B1409 <aidanb1049@gmail.com>
2025-07-19 01:37:49 -0700

    Change how file extensions are written

--- src/util/resort_datasets.py
+++ src/util/resort_datasets.py
@@ -33,7 +33,7 @@ def main():
             # test records are in format "test_pixel_annotations" / food_type / id_num
             food_type, id_num = record.split("/")[-1:-2]
             for file_type, ext in folders:
-                record_basename = f"{food_type}-{id_num}.{ext}"
+                record_basename = f"{food_type}-{id_num}"
                 # move images
                 shutil.copy(
                     os.path.join(ds_path, file_type, record_basename),
