d9136e30d5abb4ac3c9145f14cf270ec7891d931
Aidan-B1409 <aidanb1049@gmail.com>
2025-07-19 01:29:25 -0700

    Add dataset conversion for food201

--- src/util/resort_datasets.py
+++ src/util/resort_datasets.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 import pandas as pd
 
@@ -11,5 +12,28 @@ def main():
     test_df = pd.read_csv(
         os.path.join(ds_path, "food201", "test_pixel_annotations.txt")
     )
+    train_df = pd.read_csv(
+        os.path.join(ds_path, "food201", "train_pixel_annotations.txt")
+    )
+    test_dir = os.path.join(ds_path, "test")
+    train_dir = os.path.join(ds_path, "train")
+    folders = [("masks", ".png"), ("images", ".jpg"), ("labels", ".txt")]
+    for dir, _ in folders:
+        os.makedirs(os.path.join(test_dir, dir), exist_ok=True)
     # for each file in list
-    # format is
+    # format is test_pixel_annotations/food_type/id_num
+    # target format is food201/(data_type)/food_type - id_num . ext
+    # extension is .jpg for images, .png for masks, .txt for labels
+    # target directories are food201/images, food201/labels, food201/masks
+
+    # Read each file path from dataframe
+    for dest_folder, dest_df in zip([train_dir, test_dir], [train_df, test_df]):
+        for record in dest_df:
+            _, food_type, id_num = record.split("/")
+            for file_type, ext in folders:
+                record_basename = f"{food_type}-{id_num}.{ext}"
+                # move images
+                shutil.move(
+                    os.path.join(ds_path, file_type, record_basename),
+                    os.path.join(test_dir, file_type, record_basename),
+                )
