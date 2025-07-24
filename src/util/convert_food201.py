556e9a782e816e328a97477c65bab4dc40d35903
Aidan-B1409 <aidanb1049@gmail.com>
2025-07-24 02:33:48 -0700

    Add script for converstion food201 indices

---
+++ src/util/convert_food201.py
@@ -0,0 +1,35 @@
+import os
+
+import pandas as pd
+
+
+def main():
+    root_pth = "/nfs/guille/eecs_research/soundbendor/beerya/food_datasets/food201/"
+    origin_label_key = pd.read_csv(
+        os.path.join(root_pth, "food201/pixel_annotation_map.csv"),
+        columns=["id", "label"],
+        header=None,
+    )
+    normalized_label_key = pd.read_csv(
+        os.path.join(root_pth, "food201/multilabel_map.csv"),
+        columns=["id", "label"],
+        header=None,
+    )
+    # [test, train]
+    conversion_key = dict(zip(origin_label_key["id"], normalized_label_key["id"]))
+    for dir in os.listdir(os.path.join(root_pth, "data")):
+        # [images, ...]
+        for box_pth in os.listdir(os.path.join(dir, "bboxes")):
+            # modify bounding box
+            data = []
+            with open(box_pth, "r") as f:
+                for line in f.readlines():
+                    line_arr = f.split()
+                    line_arr[0] = conversion_key[line_arr[0]]
+                    data.append(" ".join(line_arr))
+            with open(box_pth, "w") as f:
+                f.writelines(data)
+
+
+if __name__ == "__main__":
+    main()
