35eb1b129a7eabe316dab4973e56e95b478c0ee1
Aidan-B1409 <aidanb1049@gmail.com>
2025-07-24 00:53:36 -0700

    Fix import for foodx251

--- src/data_wrappers/foodx251.py
+++ src/data_wrappers/foodx251.py
@@ -3,12 +3,13 @@ from typing import List
 
 import pandas as pd
 
-from .data_wrappers import Dataset
+from .dataset import Dataset
 
 # root
 # --- annot
 # --- --- class_list.txt
 # --- --- test_info.csv
+
 
 class FoodX251(Dataset):
     def __init__(self, root: str) -> None:
@@ -20,23 +21,28 @@ class FoodX251(Dataset):
         self.val_path = os.path.join(root, "annot/val_info.csv")
 
     def get_class_list(self) -> List[str]:
-        return self.cmap['label']
+        return self.cmap["label"]
 
     def _naturalize_class_labels(self, row: str) -> str:
-        return row.capitalize().replace('_', ' ')
+        return row.capitalize().replace("_", " ")
 
     def _get_classmap(self) -> pd.DataFrame:
-        cmap = pd.read_csv(os.path.join(self.root, "annot/class_list.txt"), delim_whitespace=True, index_col=0, names=['id', 'label'])
-        cmap['label'] = cmap['label'].apply(self._naturalize_class_labels)
+        cmap = pd.read_csv(
+            os.path.join(self.root, "annot/class_list.txt"),
+            delim_whitespace=True,
+            index_col=0,
+            names=["id", "label"],
+        )
+        cmap["label"] = cmap["label"].apply(self._naturalize_class_labels)
         return cmap
 
     def _map_class(self, row: str):
-        return self.cmap.loc[int(row)]['label']
+        return self.cmap.loc[int(row)]["label"]
 
     def _get_dataset(self, pth: str):
-        data = pd.read_csv(pth, names=['fname', 'class'])
+        data = pd.read_csv(pth, names=["fname", "class"])
         # substitute id numbers with class names
-        data['class'] = data['class'].apply(self._map_class)
+        data["class"] = data["class"].apply(self._map_class)
         return data
 
     # WARN: need to fix, csv is different
