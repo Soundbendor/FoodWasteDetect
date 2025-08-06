d21837952be0ef4462377550df85fe1dd5a0256b
Aidan-B1409 <aidanb1049@gmail.com>
2025-08-05 18:35:05 -0700

    Change payload from expecting pandas Series to list

--- src/database/vecdb.py
+++ src/database/vecdb.py
@@ -69,7 +69,7 @@ class VectorDB:
             payload = {"class": food_type}
             if metadata:
                 for k, v in metadata.items():
-                    payload[k] = v.iloc[idx]
+                    payload[k] = v[idx]
             if not ids:
                 img_num = int(os.path.splitext(payload["img_path"])[0].split("_")[1])
             else:
