4bd1664b85c144cb31c7c2cc3692aa47e9ef5e05
Aidan-B1409 <aidanb1049@gmail.com>
2025-08-10 11:34:44 -0700

    Change foodx251 cmap to dataframe

--- src/data_wrappers/foodx251.py
+++ src/data_wrappers/foodx251.py
@@ -18,7 +18,7 @@ class FoodX251(Dataset):
         # WARN: test_path csv does not have class labels
         self.test_path = os.path.join(root, "annot/test_info.csv")
         self.val_path = os.path.join(root, "annot/val_info.csv")
-        self.cmap = self._get_classmap()["label"]
+        self.cmap = self._get_classmap()
 
     # WARN: This is returning pd.Series
     def get_class_list(self) -> List[str]:
