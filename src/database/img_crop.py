b8de7486a87643e757342a739b280a1b657287e4
Aidan-B1409 <aidanb1049@gmail.com>
2025-08-11 12:46:36 -0700

    Reverse indexer

--- src/database/img_crop.py
+++ src/database/img_crop.py
@@ -36,7 +36,7 @@ class ImageCropper:
             results = self.model.process(img)
             cv2.destroyAllWindows()
             del img
-            init_crop_idx = prev_total_crops - self.model.crop_idx
+            init_crop_idx = self.model.crop_idx - prev_total_crops
             # for each detection
             for crop_idx in range(init_crop_idx, self.model.crop_idx):
                 record = {'patch_name': f"crop_{crop_idx}.jpg", 
