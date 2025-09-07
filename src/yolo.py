8fc55b5eb2d10c78a248ba5ec9499899ce320a78
Aidan-B1409 <aidanb1049@gmail.com>
2025-09-07 16:49:03 -0700

    Fix class row header in experiment

--- src/yolo.py
+++ src/yolo.py
@@ -135,11 +135,11 @@ def insert_class_labels():
         query_vec = clip_embedder.get_embedding([eval_patch])[0]
         candidate_vecs = db.query(None, query_vec)
         # score based on if image class is detected at all
-        score, confidence, prediction = db.score(candidate_vecs, row["class"], "voting")
-        top5_score, _, _ = db.score(candidate_vecs, row["class"], "top5")
-        metric.update_scores(candidate_vecs, db, row["class"])
+        score, confidence, prediction = db.score(candidate_vecs, row["label"], "voting")
+        top5_score, _, _ = db.score(candidate_vecs, row["label"], "top5")
+        metric.update_scores(candidate_vecs, db, row["label"])
         logging.info(f"Predicted Label: {prediction}")
-        logging.info(f"True label: {row['class']}")
+        logging.info(f"True label: {row['label']}")
         logging.info(f"In Top 5? {top5_score}")
         logging.info(f"Current Accuracies: {metric.compute_accuracies()}")
 
