76e76857fd3afa780d11e8dd9a8a5687007d4115
Aidan-B1409 <aidanb1049@gmail.com>
2025-10-01 22:25:59 -0700

    Fix foodseg103 class label keys

--- src/data_wrappers/foodseg103.py
+++ src/data_wrappers/foodseg103.py
@@ -18,7 +18,7 @@ class FoodSeg103(SegDataset):
         # Load id2label.json
         with open(os.path.join(self.root, "id2label.json"), "r") as label_map:
             labels = json.load(label_map)
-        return labels
+        return {int(k): v for k,v in labels.items()}
 
     def val_set(self) -> pd.DataFrame:
         return self._load_df("validation.csv")
