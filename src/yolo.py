fc928465a66676f18c2b5384d9ca46adac3f8eb6
Aidan-B1409 <aidanb1049@gmail.com>
2025-10-02 21:50:22 -0700

    Change ID vector to list

--- src/yolo.py
+++ src/yolo.py
@@ -245,7 +245,7 @@ def test_new_datasets():
 
             # WARN: ID COLLISION BETWEEN DATASETS
             # ids = [get_id(pth) for pth in patch_pths]
-            ids = (idx * len(vectors)) + np.arange(1, len(vectors))
+            ids = list((idx * len(vectors)) + np.arange(1, len(vectors)))
             db.add_records(list(patches["class"]), vectors, metadata, ids)
 
 
