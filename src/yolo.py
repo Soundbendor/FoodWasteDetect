bb56fff830931ead3a087920bf441a830a046a22
Aidan-B1409 <aidanb1049@gmail.com>
2025-10-03 13:48:09 -0700

    Update ID search

--- src/yolo.py
+++ src/yolo.py
@@ -273,26 +273,22 @@ class ExperimentManager():
         return patch_datasets
 
     def update_vecdb(self, patch_datasets: dict[str, pd.DataFrame]):
-        ds = pd.concat(patch_datasets.values())
+        ds = pd.concat(patch_datasets.values(), ignore_index=True)
         batches = np.array_split(ds, self.BATCH_SIZE)
         for batchnum, patches in enumerate(batches):
             # TODO: check if IDs exist before rendering vectors
-            print(patches)
-            # patch_pths = [
-            #     os.path.join(ds.root, "train", "patches", x)
-            #     for x in patches["patch_name"]
-            # ]
-            # vectors = self.embedder.get_embedding(patch_pths)
-            # metadata = {
-            #     "img_path": pd.Series(patch_pths),
-            #     "src_img": patches["src_img"],
-            # }
-            #
-            # # WARN: This STILL doesn't work
-            # # idx is only within a given dataset...
-            # ids = list(map(int, (batchnum * len(vectors)) + np.arange(len(vectors))))
-            # self.db.add_records(list(patches["class"]), vectors, metadata, ids)
-            #
+            patch_pths = [
+                os.path.join(ds.root, "train", "patches", x)
+                for x in patches["patch_name"]
+            ]
+            vectors = self.embedder.get_embedding(patch_pths)
+            metadata = {
+                "img_path": pd.Series(patch_pths),
+                "src_img": patches["src_img"],
+            }
+
+            self.db.add_records(list(patches["class"]), vectors, metadata, patches.index)
+
             
         
 
