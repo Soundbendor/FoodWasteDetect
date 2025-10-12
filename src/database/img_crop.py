5c1d232dd20e7cdccfd134a7de31efb57d331582
Aidan-B1409 <aidanb1049@gmail.com>
2025-10-12 16:09:21 -0700

    Fix subset

--- src/database/img_crop.py
+++ src/database/img_crop.py
@@ -30,7 +30,7 @@ class ImageCropper:
         )
         prev_total_crops = 0
         # WARN: Due to new changes, this interface is now incompatible with FoodX-251.
-        for idx, img_name in subset["images"].iterrows():
+        for img_name in subset["images"]:
             img_pth = os.path.join(ds_path, img_name)
             # load image
             img = cv2.imread(img_pth)
