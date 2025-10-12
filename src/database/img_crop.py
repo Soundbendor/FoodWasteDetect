be999ae3b0076b16e97afe4dc3ac8dbe1e55894c
Aidan-B1409 <aidanb1049@gmail.com>
2025-10-12 16:07:53 -0700

    Fix ImageCropper to work with bounding box datasets by stripping out class labels from dets.csv

--- src/database/img_crop.py
+++ src/database/img_crop.py
@@ -1,14 +1,13 @@
 import os
 from typing import Callable
 
-import pandas as pd
 import cv2
+import pandas as pd
 from ultralytics import solutions
 
 
-
 class ImageCropper:
-    # WARN: This is probably a bad interface, since Dataset depends 
+    # WARN: This is probably a bad interface, since Dataset depends
     # on ImageCropper and ImageCropper depends on Dataset.
     def __init__(self, dataset, model: str) -> None:
         # TODO: Convert get_class_list to return list, rather than dataframe
@@ -26,10 +25,12 @@ class ImageCropper:
         records = []
         ds_path = os.path.join(self.ds.root, subset_dirname)
         # Update path to save crops
-        self.model.crop_dir = os.path.join(os.path.dirname(ds_path), "cropped-detections")
-        # WARN: Depends on subset (e.g. val_set) returning ['fname', 'class']
+        self.model.crop_dir = os.path.join(
+            os.path.dirname(ds_path), "cropped-detections"
+        )
         prev_total_crops = 0
-        for idx, (img_name, class_label) in subset.iterrows():
+        # WARN: Due to new changes, this interface is now incompatible with FoodX-251.
+        for idx, img_name in subset["images"].iterrows():
             img_pth = os.path.join(ds_path, img_name)
             # load image
             img = cv2.imread(img_pth)
@@ -38,14 +39,14 @@ class ImageCropper:
             del img
             # for each detection
             for crop_idx in range(prev_total_crops, self.model.crop_idx):
-                record = {'patch_name': f"crop_{crop_idx}.jpg", 
-                          "label": class_label,
-                           "source_img": img_name}
+                record = {
+                    "patch_name": f"crop_{crop_idx}.jpg",
+                    "source_img": img_name,
+                }
                 print(record)
                 records.append(record)
             prev_total_crops = self.model.crop_idx
             print(f"Cropped Images: {results.total_crop_objects}")
         df = pd.DataFrame.from_records(records)
-        # WARN: empty df
         df_path = os.path.join(self.ds.root, subset_dirname, "dets.csv")
         df.to_csv(df_path)
