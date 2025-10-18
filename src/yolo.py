505f3f352e7e48dddb237b86a02c3bb246c8fa09
Aidan-B1409 <aidanb1049@gmail.com>
2025-10-17 20:15:19 -0700

    Fix df apply

--- src/yolo.py
+++ src/yolo.py
@@ -215,7 +215,7 @@ class ExperimentManager:
     def load_imgs(self, df: pd.DataFrame) -> pd.DataFrame:
         # load all the images
         df["img_files"] = df["patch_pths"].apply(Image.open)
-        valid_imgs = df["img_files"].apply(self._check_img, axis=1)
+        valid_imgs = df["img_files"].apply(self._check_img)
         return df[valid_imgs]
 
 
