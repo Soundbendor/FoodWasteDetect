9e4e01dcc9d624bd1a360b7a1766b943f1c7059c
Aidan-B1409 <aidanb1049@gmail.com>
2025-10-17 19:39:59 -0700

    Ensure images inserted at training time are all larger than certain size

--- src/database/clip_embedding.py
+++ src/database/clip_embedding.py
@@ -12,8 +12,11 @@ class CLIPEmbedding:
             model_name, truncate_dim=vec_dim, trust_remote_code=True
         )
 
+    def _check_img(self, img: Image) -> bool:
+        return all(i >= 20 for i in img.size)
+
     def get_embedding(self, img_names: List[str]) -> torch.Tensor:
-        return self.model.encode([Image.open(filepath) for filepath in img_names])
+        return self.model.encode(list(filter(self._check_img, [Image.open(filepath) for filepath in img_names])))
 
     def get_text_embedding(self, labels: List[str]) -> torch.Tensor:
         return self.model.encode(labels)
