fc2ebff6d4c56ab1c645ed307cd3f7d56ccbec3a
Aidan-B1409 <aidanb1049@gmail.com>
2025-11-25 01:51:09 -0800

    Bypass predictions with "unknown" labels

--- src/util.py
+++ src/util.py
@@ -94,6 +94,8 @@ class ExperimentResult:
             label_ids = gt_boxes.apply(
                 lambda x: self.map_class_index(x["class_id"], x["dataset"]), axis=1
             )
+            if any(x == "Unknown" for x in gt_boxes["_id"]):
+                continue
             pred_classes = preds_df["class_id"]
             img_p, img_r = self._get_img_pr(list(label_ids), list(pred_classes))
             print(f"DEBUG - Precision: {img_p}, Recall: {img_r}")
