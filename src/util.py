4b9905c38e8486a0b1c80f213df3bb1f4a3fce10
Aidan-B1409 <aidanb1049@gmail.com>
2025-11-25 01:56:34 -0800

    fix

--- src/util.py
+++ src/util.py
@@ -94,7 +94,7 @@ class ExperimentResult:
             label_ids = gt_boxes.apply(
                 lambda x: self.map_class_index(x["class_id"], x["dataset"]), axis=1
             )
-            if type(gt_boxes["class_id"]) == pd.Series:
+            if any(type(x) == pd.Series for x in gt_boxes["class_id"]):
                 continue
             pred_classes = preds_df["class_id"]
             img_p, img_r = self._get_img_pr(list(label_ids), list(pred_classes))
