3ecab00a7f8c2fccac19546677cf9cadb91853ad
Aidan-B1409 <aidanb1049@gmail.com>
2026-01-13 13:08:08 -0800

    Update experiment report

--- src/experiment_report.py
+++ src/experiment_report.py
@@ -24,7 +24,7 @@ class ExperimentReport:
         union_area = box1_area + box2_area - inter_area
         return inter_area / union_area
 
-    def get_pr(self) -> tuple[float, float]:
+    def get_pr(self, iou_threshold: float = 0.50) -> tuple[float, float]:
         """
         At the end of an experiment, calculate the classwise precision and recall
         """
@@ -36,4 +36,36 @@ class ExperimentReport:
             # Where preds and truths here refer to the boxes for this image
             preds = self.pred[img_name]
             truths = self.actual[img_name]
+            # Total number of positives is equal to the number of ground truth labels in the dataset
+            total_positive += len(truths)
             # To compute p/r, we only compute IOU in cases of matching class IDs
+            # Sort predictons  by confidence 
+            preds = preds.sort_values(by="conf")
+            for _, pred in preds.iterrows():
+                matched = False
+                # For each remaining detection
+                for gt_idx, gt in truths:
+                    if pred["class_id"] == gt["class_id"]:
+                        iou = self.compute_iou(pred["box"], gt["box"])
+                        if iou >= iou_threshold:
+                            true_positive += 1
+                            truths.drop(gt_idx)
+                            matched = True
+                            break
+                # If none of the ground truths match this prediction, it is a false detection.
+                if not matched:
+                    false_positive += 1
+        # TODO: Prevent divide by zero errors
+        precision = true_positive / (true_positive + false_positive)
+        recall = true_positive / total_positive
+        return precision, recall
+
+    def get_map50(self) -> float:
+        # TODO: Implement map50 calculation
+        pass
+
+
+
+                            
+
+                
