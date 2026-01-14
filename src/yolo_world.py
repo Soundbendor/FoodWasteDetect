4ed477fc8ad38cc10da72c6f296f45faf0d6cd9d
Aidan-B1409 <aidanb1049@gmail.com>
2026-01-13 18:56:51 -0800

    add debug print

--- src/yolo_world.py
+++ src/yolo_world.py
@@ -41,6 +41,7 @@ def main():
     for img_name in ds_img_keys:
         img_pth = os.path.join(ds_path, SUBSET, "images", img_name)
         results = model.predict(img_pth)
+        print(results)
         pred_ds[img_name] = get_predicted_detections(results)
 
     # Initialize metrics
