18244be4040a4c337520d0367d102aad97aa7862
Aidan-B1409 <aidanb1049@gmail.com>
2026-01-13 17:24:49 -0800

    Breaking: Deprecate ExperimentResult

--- src/util.py
+++ src/util.py
@@ -7,10 +7,8 @@ from collections import defaultdict
 import numpy as np
 import pandas as pd
 import yaml
-from mean_average_precision import MetricBuilder
 from pandas.core.groupby import DataFrameGroupBy
 from qdrant_client.models import ScoredPoint
-from sklearn.metrics import average_precision_score
 
 from database.vecdb import VectorDB
 
@@ -31,209 +29,208 @@ class EvalMetric:
     def compute_accuracies(self):
         return self.scores / self.len
 
-
-# WARN: Should deprecate. Out of date, buggy, brittle interface.
-# Should replace with ExperimentReport() going forward.
-class ExperimentResult:
-    # INFO: Expects preds, results saved in df, loaded and grouped by file name
-    def __init__(
-        self,
-        preds: DataFrameGroupBy,
-        truths: DataFrameGroupBy,
-        n_classes: int,
-        cmap: pd.DataFrame,
-    ):
-        # Each element of (preds, truths) represents 1 image
-        # Contains
-        self.preds = preds
-        self.truths = truths
-        self.total_acc_count = 0
-        self.n_classes = n_classes
-        self.ds_map = {
-            "foodseg103": pd.read_csv("src/util/foodseg103_map.csv", index_col=0),
-            "uecfoodpix": pd.read_csv("src/util/uec_map.csv", index_col=0),
-        }
-        self.cmap = cmap
-        # TODO: How should we map ids to text labels?
-        #
-
-    def map_class_index(self, class_id: int, ds_name: str) -> int:
-        """
-        Given a class label and a dataset, return the correct class ID in the unified dataset space
-        """
-        if ds_name == "food201":
-            return class_id
-        return self.ds_map[ds_name].iloc[class_id]["new_id"]
-
-    def _unpack_gt_box(self, coords: str) -> list[int]:
-        return list(map(float, coords[1:-1].split()))
-
-    def get_pr(self, iou_threshold=0.5, use_txt=False) -> tuple[float, float]:
-
-        def compute_iou(box1, box2):
-            x1, y1, x2, y2 = box1
-            x1g, y1g, x2g, y2g = box2
-
-            xi1 = max(x1, x1g)
-            yi1 = max(y1, y1g)
-            xi2 = min(x2, x2g)
-            yi2 = min(y2, y2g)
-            inter_area = max(0, xi2 - xi1) * max(0, yi2 - yi1)
-
-            box1_area = (x2 - x1) * (y2 - y1)
-            box2_area = (x2g - x1g) * (y2g - y1g)
-            union_area = box1_area + box2_area - inter_area
-            return inter_area / union_area
-
-        tp = 0
-        fp = 0
-        positives = 0
-        for img_name, preds_df in self.preds:
-            preds = []
-            gt = []
-            try:
-                gt_boxes = self.truths.get_group(os.path.splitext(img_name)[0])
-            except Exception:
-                print(f"File {img_name} not found in the label set!")
-                continue
-            # Re-map ground truth class IDs
-            if not use_txt:
-                label_ids = gt_boxes.apply(
-                    lambda x: self.map_class_index(x["class_id"], x["dataset"]), axis=1
-                )
-            if any(type(x) == pd.Series for x in gt_boxes["class_id"]):
-                continue
-
-            for idx, box in gt_boxes.iterrows():
-                # [x1, y1, x2, y2, label_id]
-                if use_txt:
-                    gt.append([*self._unpack_gt_box(box["box_coords"]), box["class"]])
-                else:
-                    gt.append([*self._unpack_gt_box(box["box_coords"]), label_ids[idx]])
-
-            for idx, pred in preds_df.iterrows():
-                # [x1, y1, x2, y2, label_id, confidence]
-                if use_txt:
-                    preds.append(
-                        [
-                            *ast.literal_eval(pred["xyxy"])[0],
-                            pred["class_name"].strip().lower(),
-                            pred["conf"],
-                        ]
-                    )
-                else:
-                    preds.append(
-                        [
-                            *ast.literal_eval(pred["xyxy"])[0],
-                            pred["class_id"].strip().lower(),
-                            pred["conf"],
-                        ]
-                    )
-
-            preds.sort(key=lambda x: x[5], reverse=True)
-            used = [False] * len(gt)
-            positives += len(gt)
-            # Used for keeping track of tp/fp for mAP score
-            for detection in preds:
-                pred_id = detection[4]
-                matched = False
-                for idx, a in enumerate(gt):
-                    gt_id = a[4]
-                    if used[idx]:
-                        continue
-
-                    iou = compute_iou(detection[:4], a[:4])
-                    if gt_id == pred_id:
-                        tp += 1
-                        used[idx] = True
-                        matched = True
-                        break
-                if not matched:
-                    fp += 1
-
-        precision = tp / (tp + fp) if (tp + fp) > 0 else 0
-        recall = tp / positives if positives > 0 else 0
-        return precision, recall
-
-    # def get_map50_95(self) -> float:
-    #     maps = []
-    #     for iou_threshold in np.arange(0.50, 1, step=0.05):
-    #         map, _, _ = self.new_map50(iou_threshold)
-    #     return np.mean(maps)
-
-    def get_map50(self, use_txt=False) -> tuple[float, float]:
-        """
-        Calculate mAP-50 across all predictions
-        """
-
-        gt = []
-        preds = []
-        for img_name, preds_df in self.preds:
-            # Get matching ground truth boxes
-            try:
-                gt_boxes = self.truths.get_group(os.path.splitext(img_name)[0])
-            except Exception:
-                print(f"File {img_name} not found in the label set!")
-                continue
-
-            if not use_txt:
-                label_ids = gt_boxes.apply(
-                    lambda x: self.map_class_index(x["class_id"], x["dataset"]), axis=1
-                )
-            if any(type(x) == pd.Series for x in gt_boxes["class_id"]):
-                continue
-
-            class_ids = {}
-            n_ids = 0
-            for idx, box in gt_boxes.iterrows():
-                box["class"] = box["class"].strip().lower()
-                # [x1, y1, x2, y2, label_id]
-                if box["class"] not in class_ids:
-                    class_ids[box["class"]] = n_ids
-                    n_ids += 1
-                class_id = class_ids[box["class"]]
-
-                gt.append([*self._unpack_gt_box(box["box_coords"]), class_id])
-
-            for idx, pred in preds_df.iterrows():
-                # [x1, y1, x2, y2, label_id, confidence]
-                pred["class_name"] = pred["class_name"].strip().lower()
-                if pred["class_name"] not in class_ids:
-                    class_ids[pred["class_name"]] = n_ids
-                    n_ids += 1
-                class_id = class_ids[pred["class_name"]]
-
-                preds.append(
-                    [
-                        *ast.literal_eval(pred["xyxy"])[0],
-                        class_id,
-                        pred["conf"],
-                    ]
-                )
-        metric_fn = MetricBuilder.build_evaluation_metric(
-            "map_2d", async_mode=True, num_classes=self.n_classes
-        )
-        # WARN: This will certainly not work
-        # as we are passing text labels where integers are expected
-        # We should do a pass over both arrays and convert text labels to integers
-
-        metric_fn.add(np.array(preds), np.array(gt))
-
-        # compute PASCAL VOC metric at the all points
-        map50 = metric_fn.value(iou_thresholds=0.5)["mAP"]
-        map50_95 = metric_fn.value(
-            iou_thresholds=np.arange(0.5, 1.0, 0.05),
-            recall_thresholds=np.arange(0.0, 1.01, 0.01),
-            mpolicy="soft",
-        )["mAP"]
-        print(f"VOC PASCAL mAP in all points: {map50}")
-
-        # compute metric COCO metric
-        print(f"COCO mAP: {map50_95}")
-        return map50, map50_95
-
-
-def parse_args() -> argparse.Namespace:
+    # WARN: Should deprecate. Out of date, buggy, brittle interface.
+    # Should replace with ExperimentReport() going forward.
+    # class ExperimentResult:
+    #     # INFO: Expects preds, results saved in df, loaded and grouped by file name
+    #     def __init__(
+    #         self,
+    #         preds: DataFrameGroupBy,
+    #         truths: DataFrameGroupBy,
+    #         n_classes: int,
+    #         cmap: pd.DataFrame,
+    #     ):
+    #         # Each element of (preds, truths) represents 1 image
+    #         # Contains
+    #         self.preds = preds
+    #         self.truths = truths
+    #         self.total_acc_count = 0
+    #         self.n_classes = n_classes
+    #         self.ds_map = {
+    #             "foodseg103": pd.read_csv("src/util/foodseg103_map.csv", index_col=0),
+    #             "uecfoodpix": pd.read_csv("src/util/uec_map.csv", index_col=0),
+    #         }
+    #         self.cmap = cmap
+    #         # TODO: How should we map ids to text labels?
+    #         #
+    #
+    #     def map_class_index(self, class_id: int, ds_name: str) -> int:
+    #         """
+    #         Given a class label and a dataset, return the correct class ID in the unified dataset space
+    #         """
+    #         if ds_name == "food201":
+    #             return class_id
+    #         return self.ds_map[ds_name].iloc[class_id]["new_id"]
+    #
+    #     def _unpack_gt_box(self, coords: str) -> list[int]:
+    #         return list(map(float, coords[1:-1].split()))
+    #
+    #     def get_pr(self, iou_threshold=0.5, use_txt=False) -> tuple[float, float]:
+    #
+    #         def compute_iou(box1, box2):
+    #             x1, y1, x2, y2 = box1
+    #             x1g, y1g, x2g, y2g = box2
+    #
+    #             xi1 = max(x1, x1g)
+    #             yi1 = max(y1, y1g)
+    #             xi2 = min(x2, x2g)
+    #             yi2 = min(y2, y2g)
+    #             inter_area = max(0, xi2 - xi1) * max(0, yi2 - yi1)
+    #
+    #             box1_area = (x2 - x1) * (y2 - y1)
+    #             box2_area = (x2g - x1g) * (y2g - y1g)
+    #             union_area = box1_area + box2_area - inter_area
+    #             return inter_area / union_area
+    #
+    #         tp = 0
+    #         fp = 0
+    #         positives = 0
+    #         for img_name, preds_df in self.preds:
+    #             preds = []
+    #             gt = []
+    #             try:
+    #                 gt_boxes = self.truths.get_group(os.path.splitext(img_name)[0])
+    #             except Exception:
+    #                 print(f"File {img_name} not found in the label set!")
+    #                 continue
+    #             # Re-map ground truth class IDs
+    #             if not use_txt:
+    #                 label_ids = gt_boxes.apply(
+    #                     lambda x: self.map_class_index(x["class_id"], x["dataset"]), axis=1
+    #                 )
+    #             if any(type(x) == pd.Series for x in gt_boxes["class_id"]):
+    #                 continue
+    #
+    #             for idx, box in gt_boxes.iterrows():
+    #                 # [x1, y1, x2, y2, label_id]
+    #                 if use_txt:
+    #                     gt.append([*self._unpack_gt_box(box["box_coords"]), box["class"]])
+    #                 else:
+    #                     gt.append([*self._unpack_gt_box(box["box_coords"]), label_ids[idx]])
+    #
+    #             for idx, pred in preds_df.iterrows():
+    #                 # [x1, y1, x2, y2, label_id, confidence]
+    #                 if use_txt:
+    #                     preds.append(
+    #                         [
+    #                             *ast.literal_eval(pred["xyxy"])[0],
+    #                             pred["class_name"].strip().lower(),
+    #                             pred["conf"],
+    #                         ]
+    #                     )
+    #                 else:
+    #                     preds.append(
+    #                         [
+    #                             *ast.literal_eval(pred["xyxy"])[0],
+    #                             pred["class_id"].strip().lower(),
+    #                             pred["conf"],
+    #                         ]
+    #                     )
+    #
+    #             preds.sort(key=lambda x: x[5], reverse=True)
+    #             used = [False] * len(gt)
+    #             positives += len(gt)
+    #             # Used for keeping track of tp/fp for mAP score
+    #             for detection in preds:
+    #                 pred_id = detection[4]
+    #                 matched = False
+    #                 for idx, a in enumerate(gt):
+    #                     gt_id = a[4]
+    #                     if used[idx]:
+    #                         continue
+    #
+    #                     iou = compute_iou(detection[:4], a[:4])
+    #                     if gt_id == pred_id:
+    #                         tp += 1
+    #                         used[idx] = True
+    #                         matched = True
+    #                         break
+    #                 if not matched:
+    #                     fp += 1
+    #
+    #         precision = tp / (tp + fp) if (tp + fp) > 0 else 0
+    #         recall = tp / positives if positives > 0 else 0
+    #         return precision, recall
+    #
+    #     # def get_map50_95(self) -> float:
+    #     #     maps = []
+    #     #     for iou_threshold in np.arange(0.50, 1, step=0.05):
+    #     #         map, _, _ = self.new_map50(iou_threshold)
+    #     #     return np.mean(maps)
+    #
+    #     def get_map50(self, use_txt=False) -> tuple[float, float]:
+    #         """
+    #         Calculate mAP-50 across all predictions
+    #         """
+    #
+    #         gt = []
+    #         preds = []
+    #         for img_name, preds_df in self.preds:
+    #             # Get matching ground truth boxes
+    #             try:
+    #                 gt_boxes = self.truths.get_group(os.path.splitext(img_name)[0])
+    #             except Exception:
+    #                 print(f"File {img_name} not found in the label set!")
+    #                 continue
+    #
+    #             if not use_txt:
+    #                 label_ids = gt_boxes.apply(
+    #                     lambda x: self.map_class_index(x["class_id"], x["dataset"]), axis=1
+    #                 )
+    #             if any(type(x) == pd.Series for x in gt_boxes["class_id"]):
+    #                 continue
+    #
+    #             class_ids = {}
+    #             n_ids = 0
+    #             for idx, box in gt_boxes.iterrows():
+    #                 box["class"] = box["class"].strip().lower()
+    #                 # [x1, y1, x2, y2, label_id]
+    #                 if box["class"] not in class_ids:
+    #                     class_ids[box["class"]] = n_ids
+    #                     n_ids += 1
+    #                 class_id = class_ids[box["class"]]
+    #
+    #                 gt.append([*self._unpack_gt_box(box["box_coords"]), class_id])
+    #
+    #             for idx, pred in preds_df.iterrows():
+    #                 # [x1, y1, x2, y2, label_id, confidence]
+    #                 pred["class_name"] = pred["class_name"].strip().lower()
+    #                 if pred["class_name"] not in class_ids:
+    #                     class_ids[pred["class_name"]] = n_ids
+    #                     n_ids += 1
+    #                 class_id = class_ids[pred["class_name"]]
+    #
+    #                 preds.append(
+    #                     [
+    #                         *ast.literal_eval(pred["xyxy"])[0],
+    #                         class_id,
+    #                         pred["conf"],
+    #                     ]
+    #                 )
+    #         metric_fn = MetricBuilder.build_evaluation_metric(
+    #             "map_2d", async_mode=True, num_classes=self.n_classes
+    #         )
+    #         # WARN: This will certainly not work
+    #         # as we are passing text labels where integers are expected
+    #         # We should do a pass over both arrays and convert text labels to integers
+    #
+    #         metric_fn.add(np.array(preds), np.array(gt))
+    #
+    #         # compute PASCAL VOC metric at the all points
+    #         map50 = metric_fn.value(iou_thresholds=0.5)["mAP"]
+    #         map50_95 = metric_fn.value(
+    #             iou_thresholds=np.arange(0.5, 1.0, 0.05),
+    #             recall_thresholds=np.arange(0.0, 1.01, 0.01),
+    #             mpolicy="soft",
+    #         )["mAP"]
+    #         print(f"VOC PASCAL mAP in all points: {map50}")
+    #
+    #         # compute metric COCO metric
+    #         print(f"COCO mAP: {map50_95}")
+    #         return map50, map50_95
+    #
+    #
+    # def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Intern-FW Experiment Pipeline")
     parser.add_argument("--config_file", help="Path to experiment config")
     parser.add_argument("--partition", type=int)
