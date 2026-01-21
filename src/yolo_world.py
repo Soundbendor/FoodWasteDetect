aee25c78267542973d5129e8d4a6a52764098379
Aidan-B1409 <aidanb1049@gmail.com>
2026-01-20 22:28:22 -0800

    test 26

--- src/yolo_world.py
+++ src/yolo_world.py
@@ -1,7 +1,7 @@
 import os
 
 import pandas as pd
-from ultralytics import YOLOE, YOLOWorld
+from ultralytics import YOLO, YOLOE, YOLOWorld
 from ultralytics.engine.results import Results
 from ultralytics.models.yolo.yoloe import YOLOEPESegTrainer
 
@@ -38,6 +38,26 @@ def convert_food201_class_idx(
         )
 
     return dataset, new_class_labels
+
+
+def eval_yolo_26_seg():
+    args = parse_args()
+    cfg = parse_cfg(args.config_file)
+    ds_path = cfg["paths"]["food201"]
+    ds = Food201(ds_path)
+    model = YOLO("yolo26x-seg.pt")  # or select yolov8m/l-world.pt for different sizes
+
+    # Get ground truth boxes for metrics class
+    class_names = list(ds.get_class_labels())
+    # Remove all "Unknown" values from dataset
+    gt_dataset = ds.get_box_dataset(SUBSET)
+    gt_dataset, class_names = convert_food201_class_idx(gt_dataset, class_names)
+    # model.set_classes(class_names)
+
+    results = model.train(data=cfg["paths"]["food201_yolo"], epochs=10)
+    print(results)
+    metrics = model.val()
+    print(metrics)
 
 
 def eval_yolo_e():
