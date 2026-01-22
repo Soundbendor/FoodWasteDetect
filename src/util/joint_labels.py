3384a5c1fcbb17a2d4a308e7dd057aa8f1c7c0a3
Aidan-B1409 <aidanb1049@gmail.com>
2026-01-22 14:27:38 -0800

    Update unified class map

--- src/util/joint_labels.py
+++ src/util/joint_labels.py
@@ -6,10 +6,13 @@ from functools import partial
 import pandas as pd
 
 ROOT_PTH = "/nfs/guille/eecs_research/soundbendor/beerya/food_datasets/combined_food_seg/labels/test"
+food201_map = pd.read_csv("food201.csv", index_col=1).to_dict("index")
 # load foodseg103 map
-foodseg103_map = pd.read_csv("foodseg103_map.csv").to_dict("index")
+foodseg103_map = pd.read_csv("foodseg103_map.csv", index_col=1).to_dict("index")
 # load uecfoodpix map
-uec_map = pd.read_csv("uec_map.csv").to_dict("index")
+uec_map = pd.read_csv("uec_map.csv", index_col=1).to_dict("index")
+
+dataset_csv_map = {"foodseg103": foodseg103_map, "food201": food201_map, "uec": uec_map}
 
 
 def change_id(line: str, id_map: pd.DataFrame) -> str:
@@ -22,14 +25,10 @@ for file in os.listdir(ROOT_PTH):
     fpath = os.path.join(ROOT_PTH, file)
     with open(fpath, "r") as rfile:
         lines = rfile.readlines()
-    if re.match(r"foodseg103_.*", file):
-        id_mapper = partial(change_id, id_map=foodseg103_map)
-    elif re.match(r"uec_.*", file):
-        id_mapper = partial(change_id, id_map=uec_map)
-    else:
-        continue
     if re.search(r".*NAN", file):
         continue
+    ds_name = file.split("_")[0]
+    id_mapper = partial(change_id, id_map=dataset_csv_map[ds_name])
     lines = list(map(id_mapper, lines))
     with open(fpath, "w") as wfile:
         wfile.writelines(lines)
