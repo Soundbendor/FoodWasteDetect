205dfe2c4cc204fbfe8d4dfa75916ff38ab0b61f
Aidan-B1409 <aidanb1049@gmail.com>
2026-01-23 13:38:43 -0800

    Update YOLO-E trainer

--- src/yolo_world.py
+++ src/yolo_world.py
@@ -55,22 +55,12 @@ def eval_yolo_26_seg():
 def eval_yolo_e():
     args = parse_args()
     cfg = parse_cfg(args.config_file)
-    ds_path = cfg["paths"]["food201"]
-    ds = Food201(ds_path)
+    ds_path = cfg["paths"][args.dataset]
     model = YOLOE(
         "yoloe-26x-seg.pt"
     )  # or select yolov8m/l-world.pt for different sizes
 
-    # Get ground truth boxes for metrics class
-    class_names = list(ds.get_class_labels())
-    # Remove all "Unknown" values from dataset
-    gt_dataset = ds.get_box_dataset(SUBSET)
-    gt_dataset, class_names = convert_food201_class_idx(gt_dataset, class_names)
-    model.set_classes(class_names)
-
-    results = model.train(
-        data=cfg["paths"]["food201_yolo"], epochs=10, trainer=YOLOEPESegTrainer
-    )
+    results = model.train(data=ds_path, epochs=80, trainer=YOLOEPESegTrainer)
     print(results)
     metrics = model.val()
     print(metrics)
