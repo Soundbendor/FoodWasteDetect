16868155f88138b3c678eea3e896340842f8585a
Aidan-B1409 <aidanb1049@gmail.com>
2026-02-07 16:13:30 -0800

    Update metrics

--- src/yolo_world.py
+++ src/yolo_world.py
@@ -156,7 +156,7 @@ def eval_yolo_e():
     # WARN: The interface for ExperimentReport expects a Pandas GroupBy
     # We are providing dictionaries
     metrics = ExperimentReport(None, None)
-    metrics.get_map50(test_preds, test_ds)
+    metrics.get_map50(test_preds.values(), test_ds.values())
 
 
 def train_yolo_e():
